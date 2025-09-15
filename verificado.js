9b051a783685ddd16288c74add25fd9cae8220a3
victorxx <victorsaib@gmail.com>
2025-09-15 19:01:04 -0300

    Update verificado.js

--- verificado.js
+++ verificado.js
@@ -1,36 +1,40 @@
-<div id="x" onclick="oi()" style="background-color: aqua; padding: 20px; cursor: pointer; width: 10px;">
+<!-- Botão de fechar -->
+<div id="x" onclick="oi()" style="position: fixed; top: 130px; left: 5px; background-color: red; color: white; padding: 10px; cursor: pointer; z-index: 1001;">
   x
-  <div id="mensagem" style="position: fixed; padding-top: 20px;">
-    <iframe src="https://www.espiritosanto-es.com.br/vps.html" width="300" height="400"></iframe>
-  </div>
-
-  <script>
-    // Verifica se o cookie 'meu=ok' já existe
-    let cookies = document.cookie.split(';');
-    let existe = false;
-    for (let i = 0; i < cookies.length; i++) {
-      let cookie = cookies[i].trim();
-      if (cookie.startsWith('meu=ok')) {
-        existe = true;
-        break;
-      }
-    }
-
-    // Se já existir, oculta a div
-    if (existe) {
-      document.getElementById('x').style.display = 'none';
-    }
-
-    // Função chamada ao clicar na div
-    function oi() {
-      // Oculta a div
-      document.getElementById('x').style.display = 'none';
-
-      // Cria cookie para não mostrar novamente por 1 dia
-      let data = new Date();
-      data.setTime(data.getTime() + 24 * 60 * 60 * 1000); // 1 dia
-      let expires = "expires=" + data.toUTCString();
-      document.cookie = "meu=ok; " + expires + "; path=/";
-    }
-  </script>
 </div>
+
+<!-- Conteúdo fixo na tela -->
+<div id="mensagem" style="position: fixed; bottom: 20px; left: 5px; z-index: 1000;">
+  <iframe src="https://www.espiritosanto-es.com.br/vps.html" width="300" height="400" style="border: none;"></iframe>
+</div>
+
+<script>
+  // Verifica se o cookie já existe
+  let cookies = document.cookie.split(';');
+  let existe = false;
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i].trim();
+    if (cookie.startsWith('meu=ok')) {
+      existe = true;
+      break;
+    }
+  }
+
+  // Se o cookie existir, oculta tudo
+  if (existe) {
+    document.getElementById('x').style.display = 'none';
+    document.getElementById('mensagem').style.display = 'none';
+  }
+
+  // Função ao clicar no botão "x"
+  function oi() {
+    document.getElementById('x').style.display = 'none';
+    document.getElementById('mensagem').style.display = 'none';
+
+    // Cria cookie por 1 dia
+    let data = new Date();
+    data.setTime(data.getTime() + 24 * 60 * 60 * 1000);
+    let expires = "expires=" + data.toUTCString();
+    document.cookie = "meu=ok; " + expires + "; path=/";
+  }
+</script>
