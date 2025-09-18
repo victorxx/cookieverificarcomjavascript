2dc6f43b40ef7cb5a9c331f8dcf3e255c9e70c94
victorxx <victorsaib@gmail.com>
2025-09-18 18:20:25 -0300

    Create transitandoleft.js

---
+++ transitandoleft.js
@@ -0,0 +1,18 @@
+
+
+
+<div id="vitor"
+style="position: absolute;left: 0; top:50px;"
+>VICTOR</div>
+<script>
+    let pos=0;
+    let elemento=document.getElementById('vitor');
+    let intervalo=setInterval(()=>{
+      if(pos>=300){
+        clearInterval(intervalo);
+      }else{
+        pos++;
+        elemento.style.left=pos+'px';
+      }
+    },10);
+</script>
